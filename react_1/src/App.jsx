9498a5c989feee38a0a0b8dec82334049d96ce3e
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2025-09-13 12:29:43 +0530

    Add: Implement card layout and style for Open University welcome message

--- react_1/src/App.jsx
+++ react_1/src/App.jsx
@@ -4,7 +4,11 @@ function App() {
 
   return (
     <>
-    {/* <h1>Hello !</h1> */}
+    <div className='card'>
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTQnw9HdqQlf7cTP0snTK6neS7p9YgaVhYxg&s" alt="" width="800px"/>
+    </div>
+    <h1>Hello Open University Of Sri Lanka</h1>
+    <p>Welcome to React</p>
     </>
   )
 }
