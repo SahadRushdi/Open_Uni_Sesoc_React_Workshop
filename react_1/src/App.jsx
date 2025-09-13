bf4b3d9841409464efa0d90e88d80fa2f4881433
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2025-09-13 13:19:00 +0530

    Add: Learnt React components and imports

--- react_1/src/App.jsx
+++ react_1/src/App.jsx
@@ -1,15 +1,15 @@
+import Card from './components/Card';
 import './App.css'
 
 function App() {
 
   return (
-    <>
-    <div className='card'>
-      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTQnw9HdqQlf7cTP0snTK6neS7p9YgaVhYxg&s" alt="" width="800px"/>
+    <div className="App">
+      <Card />
+      <Card />
+      <Card />
+      <Card />
     </div>
-    <h1>Hello Open University Of Sri Lanka</h1>
-    <p>Welcome to React</p>
-    </>
   )
 }
 
---
+++ react_1/src/components/Card.jsx
@@ -0,0 +1,14 @@
+function Card() {
+    return (
+        <>
+            <div className='card'>
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTQnw9HdqQlf7cTP0snTK6neS7p9YgaVhYxg&s" alt="" width="800px" />
+                <h1>Hello Open University Of Sri Lanka</h1>
+                <p>Welcome to React</p>
+            </div>
+            <br />
+        </>
+    );
+}
+
+export default Card;
