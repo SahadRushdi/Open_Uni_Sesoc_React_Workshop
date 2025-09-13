3e16e3e489bf2378eca644d5cf65eeb8e65b2c5d
javaprocoder <160300225+javaprocoder@users.noreply.github.com>
2025-09-13 13:56:03 +0530

    feat: Add react props for images

--- react_1/src/App.jsx
+++ react_1/src/App.jsx
@@ -5,8 +5,8 @@ function App() {
 
   return (
     <div className="App">
-      <Card uni="Open University Of Sri Lanka" desc="Welcome to React Props" />
-      <Card uni="University Of Colombo" desc="Welcome to React" />
+      <Card img="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTQnw9HdqQlf7cTP0snTK6neS7p9YgaVhYxg&s" uni="Open University Of Sri Lanka" desc="Welcome to React Props" />
+      <Card img="https://www.yesman.lk/assets/img/institutes/uoc_cover-1557375201.jpg" uni="University Of Colombo" desc="Welcome to React" />
       <Card uni="University Of Peradeniya" desc="Welcome to React" />
       <Card uni="University Of Moratuwa" desc="Welcome to React Props" />
     </div>
--- react_1/src/components/Card.jsx
+++ react_1/src/components/Card.jsx
@@ -2,7 +2,7 @@ function Card(props) {
     return (
         <>
             <div className='card'>
-                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTTQnw9HdqQlf7cTP0snTK6neS7p9YgaVhYxg&s" alt="" width="800px" />
+                <img src={props.img} alt="" width="800px" />
                 <h1>Hello {props.uni}</h1>
                 <p>{props.desc}</p>
             </div>
